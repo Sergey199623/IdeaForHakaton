9a97c16bcdc1ff86a4cfb7acf4691c0b9ff4965f
sergeybelyakov <belyakovsergeyrus@yandex.ru>
2021-03-13 13:11:51 +0300

    UPD: прикручены зависимости и адаптеры, добавлены ссылки на аватарки для "соцсетей"

--- app/src/main/java/com/sv/nfcreader/data/Account.kt
+++ app/src/main/java/com/sv/nfcreader/data/Account.kt
@@ -1,6 +1,9 @@
 package com.sv.nfcreader.data
 
 abstract class Account {
+
+    abstract val avatar: String
     abstract val id: Int
     abstract val path: String
+
 }
--- app/src/main/java/com/sv/nfcreader/data/repo/Repository.kt
+++ app/src/main/java/com/sv/nfcreader/data/repo/Repository.kt
@@ -1,13 +1,16 @@
 package com.sv.nfcreader.data.repo
 
-import com.sv.nfcreader.data.*
+import com.sv.nfcreader.data.Account
+import com.sv.nfcreader.data.Facebook
+import com.sv.nfcreader.data.Twitter
+import com.sv.nfcreader.data.Vk
 
-object Repository {
+class Repository {
     fun getAccounts(): List<Account> {
         return listOf(
-            AccountTemp(1, "https://vk.com/durov"),
-            AccountTemp(2, "https://ru-ru.facebook.com/durov"),
-            AccountTemp(3, "https://twitter.com/durov")
+            Vk("https://ibb.co/KDBVgX3", 1, "https://vk.com/durov"),
+            Facebook("https://ibb.co/LN979kn",2, "https://ru-ru.facebook.com/durov"),
+            Twitter("https://ibb.co/sVG9qhp",3, "https://twitter.com/durov")
         )
     }
 }
