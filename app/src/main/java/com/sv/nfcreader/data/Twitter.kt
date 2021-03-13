760f17b9f79eeb63f911a7d471ee40deac948bb7
andreilemdyanov <lemdyanov5@mail.ru>
2021-03-13 09:58:36 +0300

    Data классы.

--- app/src/main/java/com/sv/nfcreader/data/Twitter.kt
+++ app/src/main/java/com/sv/nfcreader/data/Twitter.kt
@@ -1,4 +1,4 @@
 package com.sv.nfcreader.data
 
-class Twitter(override val id: Int, override val path: String) : Account() {
+data class Twitter(override val id: Int, override val path: String) : Account() {
 }
